425f1d9f178a6d756ed20dc7491c31bd9da1c8ee
cider-sk <perfect_uni@ezweb.ne.jp>
2016-01-13 18:29:19 +0900

    fix

--- js/custom.js
+++ js/custom.js
@@ -4,17 +4,6 @@ $(function(){
         $("html,body").animate({scrollTop:0},"fast");
     });
     
-    //日付のセレクトボックス（不要になるかも）
-    for(var i=0; i<11; i++){
-        $(".year").append($("<option>").val(i+2015).text(i+2015+"年"));
-    }
-    for(var j=1; j<13; j++){
-        $(".month").append($("<option>").val(j).text(j+"月"));
-    }
-    for(var k=1; k<32; k++){
-        $(".day").append($("<option>").val(k).text(k+"日"));
-    }
-    
     //メールフォームなどの日付部分のラジオボタンの制御
     $('[name=emailStart]').click(function(){
         if ($('input[name=emailStart]:checked').val() == 'immediately') {
